d505a35115461f0ad0c8b63b8a125de59ed6330a
Daniele410 <miraglia.daniele410@gmail.com>
2024-12-23 15:57:13 +0100

    add header component and other smalls fixs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app.routes';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AddProductComponent } from './components/add-product/add-product.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'products', pathMatch: 'full' }, // Rotta principale
+  { path: '', redirectTo: 'products', pathMatch: 'full' }, // Rotta principale
   { path: 'products', component: ProductListComponent },   // Lista prodotti
   { path: 'add-product', component: AddProductComponent }, // Aggiungi prodotto
   { path: '**', redirectTo: 'products' }                  // Fallback route
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
 })
 export class AppRoutingModule { }

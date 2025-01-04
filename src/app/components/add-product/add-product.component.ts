21b02b2cda07d33ee48e9f25b284968a7621b219
Daniele410 <miraglia.daniele410@gmail.com>
2025-01-04 18:40:10 +0100

    Add style in addProduct, header, productList, productForm

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-product',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
@@ -17,11 +21,10 @@ export class AddProductComponent {
   saveProduct(): void {
     this.productService.addProduct(this.product).subscribe({
       next: () => {
-        alert('Prodotto aggiunto con successo!');
-        this.product = {id:'', name: '', description: '', price: 0 }; // Reset del form
+        alert('add product whit success!');
+        this.product = { id: '', name: '', description: '', price: 0 }; // Reset form
       },
       error: (err) => console.error(err)
     });
   }
-
 }
